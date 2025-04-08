85ac9dfdad3a13913cde808f13448d6717db9eb5
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-04-09 02:40:31 +0900

    1st lecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); //앱 구동시켜달라는 뜻
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key} : super(key: key);
-
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-    home;
+      //home: Text('안녕') -> 글자 위젯
+      //home: Icon(Icons.star) -> 아이콘 위젯
+      //home: Image.asset('assets/dog.png')
+      //home: Container( width: 50, height: 50, color: Colors.blue )
+      home: Center(
+        child: Container( width: 50, height:50, color: Colors.blue )
+      )
     );
   }
 }
